43551a5da9a9d19738d824be2569a1af72261ad7
wilton kruszciako <103961130+Wiltonkru2022@users.noreply.github.com>
2026-08-19 05:40:31 -0300

    padroniza marca do app profissional

--- app/app-profissional/head.tsx
+++ app/app-profissional/head.tsx
@@ -2,9 +2,9 @@ export default function Head() {
   return (
     <>
       <link rel="manifest" href="/app-profissional-manifest.webmanifest" />
-      <meta name="application-name" content="SalãoPremium Profissional" />
+      <meta name="application-name" content="Salão Premiun Profissional" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta name="apple-mobile-web-app-title" content="SalaoPro" />
+      <meta name="apple-mobile-web-app-title" content="Salão Premiun Pro" />
       <meta name="theme-color" content="#09090b" />
     </>
   );
