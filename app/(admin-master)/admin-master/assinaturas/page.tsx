8af6e98e02000da4602f8258d073e1dcd1e2121d
wilton kruszciako <103961130+Wiltonkru2022@users.noreply.github.com>
2026-08-25 02:03:32 -0300

    ui: migra Assinaturas para padrão interno unificado

--- app/(admin-master)/admin-master/assinaturas/page.tsx
+++ app/(admin-master)/admin-master/assinaturas/page.tsx
@@ -1,9 +1,9 @@
-import { AdminSectionView } from "@/components/admin-master/AdminMasterViews";
+import AdminMasterSectionSimple from "@/components/admin-master/AdminMasterSectionSimple";
 import { getAdminMasterSection } from "@/lib/admin-master/data";
 
 export const dynamic = "force-dynamic";
 
 export default async function AdminMasterAssinaturasPage() {
   const data = await getAdminMasterSection("assinaturas");
-  return <AdminSectionView data={data} />;
+  return <AdminMasterSectionSimple data={data} />;
 }
