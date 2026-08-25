5487d42b74e321ca76a0120fc7e64be8106cdad4
wilton kruszciako <103961130+Wiltonkru2022@users.noreply.github.com>
2026-08-25 02:03:43 -0300

    ui: redefine Alertas como fila de ação

--- app/(admin-master)/admin-master/alertas/page.tsx
+++ app/(admin-master)/admin-master/alertas/page.tsx
@@ -1,9 +1,9 @@
-import { AdminSectionView } from "@/components/admin-master/AdminMasterViews";
+import AdminMasterSectionSimple from "@/components/admin-master/AdminMasterSectionSimple";
 import { getAdminMasterSection } from "@/lib/admin-master/data";
 
 export const dynamic = "force-dynamic";
 
 export default async function AdminMasterAlertasPage() {
   const data = await getAdminMasterSection("alertas");
-  return <AdminSectionView data={data} />;
+  return <AdminMasterSectionSimple data={{ ...data, title: "Alertas que exigem ação", description: "Fila operacional de problemas que precisam de acompanhamento. A visão geral da estabilidade fica em Saúde do sistema." }} />;
 }
