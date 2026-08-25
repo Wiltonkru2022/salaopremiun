1d7ff785aa7a90c78ca6c1e8095e12318be27e9d
wilton kruszciako <103961130+Wiltonkru2022@users.noreply.github.com>
2026-08-25 02:09:13 -0300

    ui: migra Notificações para padrão interno unificado

--- app/(admin-master)/admin-master/notificacoes/page.tsx
+++ app/(admin-master)/admin-master/notificacoes/page.tsx
@@ -1,9 +1,17 @@
-import { AdminSectionView } from "@/components/admin-master/AdminMasterViews";
+import AdminMasterSectionSimple from "@/components/admin-master/AdminMasterSectionSimple";
 import { getAdminMasterSection } from "@/lib/admin-master/data";
 
 export const dynamic = "force-dynamic";
 
 export default async function AdminMasterNotificacoesPage() {
   const data = await getAdminMasterSection("notificacoes");
-  return <AdminSectionView data={data} />;
+  return (
+    <AdminMasterSectionSimple
+      data={{
+        ...data,
+        title: "Notificações",
+        description: "Envios, histórico e falhas de comunicação em uma subárea de Campanhas.",
+      }}
+    />
+  );
 }
