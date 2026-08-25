db643bf4b9c2fadd9eeb3bcffbac067edc38e163
wilton kruszciako <103961130+Wiltonkru2022@users.noreply.github.com>
2026-08-25 02:09:44 -0300

    ui: migra Salões para padrão interno unificado

--- app/(admin-master)/admin-master/saloes/page.tsx
+++ app/(admin-master)/admin-master/saloes/page.tsx
@@ -1,9 +1,16 @@
-import { AdminSectionView } from "@/components/admin-master/AdminMasterViews";
+import AdminMasterSectionSimple from "@/components/admin-master/AdminMasterSectionSimple";
 import { getAdminMasterSection } from "@/lib/admin-master/data";
 
 export const dynamic = "force-dynamic";
 
 export default async function AdminMasterSaloesPage() {
   const data = await getAdminMasterSection("saloes");
-  return <AdminSectionView data={data} />;
+  return (
+    <AdminMasterSectionSimple
+      data={{
+        ...data,
+        description: "Contas, situação e uso da plataforma. Abra um salão para acessar o Raio-X organizado em abas.",
+      }}
+    />
+  );
 }
