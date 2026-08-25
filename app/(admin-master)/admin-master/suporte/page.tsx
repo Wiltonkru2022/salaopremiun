b44032f7e3826e4b3e91c40df6d9e0de78362dcb
wilton kruszciako <103961130+Wiltonkru2022@users.noreply.github.com>
2026-08-25 02:03:37 -0300

    ui: integra Suporte ao padrão de Atendimento

--- app/(admin-master)/admin-master/suporte/page.tsx
+++ app/(admin-master)/admin-master/suporte/page.tsx
@@ -1,9 +1,9 @@
-import { AdminSectionView } from "@/components/admin-master/AdminMasterViews";
+import AdminMasterSectionSimple from "@/components/admin-master/AdminMasterSectionSimple";
 import { getAdminMasterSection } from "@/lib/admin-master/data";
 
 export const dynamic = "force-dynamic";
 
 export default async function AdminMasterSuportePage() {
   const data = await getAdminMasterSection("suporte");
-  return <AdminSectionView data={data} />;
+  return <AdminMasterSectionSimple data={{ ...data, title: "Visão do suporte", description: "Backlog, prioridades e sinais do atendimento. Use Tickets para trabalhar cada solicitação." }} />;
 }
